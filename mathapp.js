1023dd91da6d8441bd14e446c545986b8f94bf13
Johan Karlsson <Inga@Ingas-MacBook-Pro.local>
2013-11-24 16:08:02 +0100

    Swedish + Animations
    Switched language to Swedish
    Added reference to angular-animate.js
    Added CSS transitions for animating the score each time it is increased.
    Moved the message (Correct/Try again) to the top.
    Added animation toggle

--- mathapp.js
+++ mathapp.js
@@ -1,40 +1,52 @@
+var myApp = angular.module('MathApp', ['ngAnimate']);
+
 function MathAppCtrl($scope) {
-  $scope.getRandomNumber = function () {
-    return $scope.getRandomNumberWithMax(10);
-  };
+    $scope.getRandomNumber = function() {
+        return $scope.getRandomNumberWithMax(10);
+    };
 
-  $scope.getRandomNumberWithMax = function (max) {
-    return Math.floor((Math.random()*max)+1);
-  };
+    $scope.getRandomNumberWithMax = function(max) {
+        return Math.floor((Math.random() * max) + 1);
+    };
 
-  $scope.playRandomly = function() {
-      var random = $scope.getRandomNumber();
-      if(random > 0 && random <= 2) {
-          var snd = new Audio(random.toString() + ".wav");
-          snd.play();      
-      }
-  }
-  
-  // Initial state
-  $scope.first = $scope.getRandomNumber();
-  $scope.second = $scope.getRandomNumber();
-  $scope.nrOfCorrectAnswers = 0;
+    $scope.playRandomly = function() {
+        var random = $scope.getRandomNumber();
+        if (random > 0 && random <= 2) {
+            var snd = new Audio(random.toString() + ".wav");
+            snd.play();
+        }
+    }
 
-  $scope.answer = function (){
-    return $scope.first + $scope.second; 
-  };
+    // Initial state
+    $scope.first = $scope.getRandomNumber();
+    $scope.second = $scope.getRandomNumber();
+    $scope.nrOfCorrectAnswers = 0;
+    $scope.animationToggle = '';
 
-  $scope.checkAnswer = function (){
-	// Using evil twins for comparing a number with a string
-    if($scope.givenAnswer == $scope.answer()){
-      $scope.message = 'Correct!';
-      $scope.first = $scope.getRandomNumber();
-      $scope.second = $scope.getRandomNumber();
-      $scope.nrOfCorrectAnswers++;  
-      $scope.playRandomly();
-    } else {
-      $scope.message = 'Try again!';
-    } 	
-    $scope.givenAnswer = '';
-  };
+    $scope.answer = function() {
+        return $scope.first + $scope.second;
+    };
+
+    $scope.doToggleAnimation = function() {
+        if($scope.animationToggle === '') {
+            $scope.animationToggle = 'pointsAnimation';
+        } else {
+            $scope.animationToggle = '';
+        } 
+    };
+
+    $scope.checkAnswer = function() {
+        // Using evil twins for comparing a number with a string
+        if ($scope.givenAnswer == $scope.answer()) {
+            $scope.message = 'Rätt!';
+            $scope.first = $scope.getRandomNumber();
+            $scope.second = $scope.getRandomNumber();
+            $scope.nrOfCorrectAnswers++;
+            $scope.doToggleAnimation();
+            $scope.playRandomly();
+        } else {
+            $scope.message = 'Försök igen!';
+        }
+        $scope.givenAnswer = '';
+    };
 }
