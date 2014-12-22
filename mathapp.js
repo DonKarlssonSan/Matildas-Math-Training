cd1bcec04721c4ff81026c3671dc897025c2fa25
Johan Karlsson <johan.mikael.karlsson@gmail.com>
2014-12-22 23:49:01 +0100

    Added level setting
    Adding level increase/decrease buttons
    Adding current level info
    Fixing som JSLint Problems

--- mathapp.js
+++ mathapp.js
@@ -1,46 +1,73 @@
+'use strict';
 var myApp = angular.module('MathApp', ['ngAnimate']);
 
 function MathAppCtrl($scope) {
-    $scope.getRandomNumber = function() {
-        return $scope.getRandomNumberWithMax(10);
+    $scope.getRandomNumber = function () {
+        var max = 0;
+        if ($scope.level === 1) {
+            max = 10;
+        } else if ($scope.level === 2) {
+            max = 20;
+        } else if ($scope.level === 3) {
+            max = 100;
+        }
+        return $scope.getRandomNumberWithMax(max);
     };
 
-    $scope.getRandomNumberWithMax = function(max) {
+    $scope.getRandomNumberWithMax = function (max) {
         return Math.floor((Math.random() * max) + 1);
     };
-
-    $scope.playRandomly = function() {
-        var random = $scope.getRandomNumberWithMax(4);
-        if (random > 0 && random <= 2) {
-            var snd = new Audio(random.toString() + ".wav");
-            snd.play();
-        }
-    }
-
-    // Initial state
-    $scope.first = $scope.getRandomNumber();
-    $scope.second = $scope.getRandomNumber();
-    $scope.nrOfCorrectAnswers = 0;
-    $scope.animationToggle = '';
-
-    $scope.answer = function() {
-        return $scope.first + $scope.second;
+    
+    $scope.getNewNumbers = function () {
+        $scope.first = $scope.getRandomNumber();
+        $scope.second = $scope.getRandomNumber();
     };
 
-    $scope.doToggleAnimation = function() {
-        if($scope.animationToggle === '') {
+    $scope.playRandomly = function () {
+        var random = $scope.getRandomNumberWithMax(4);
+        if (random > 0 && random <= 2) {
+            var snd = new Audio(random.toString() + '.wav');
+            snd.play();
+        }
+    };
+
+    // Initial state
+    $scope.getNewNumbers();
+    $scope.nrOfCorrectAnswers = 0;
+    $scope.animationToggle = '';
+    $scope.level = 1; // 1: easy 1-10, 2: medium: 1-20, hard: 1-100
+
+    $scope.answer = function () {
+        return $scope.first + $scope.second;
+    };
+    
+    $scope.increaseLevel = function () {
+        if ($scope.level < 3) {
+            $scope.level++;
+            $scope.getNewNumbers();
+        }
+    };
+
+    $scope.decreaseLevel = function () {
+        if ($scope.level > 1) {
+            $scope.level--;
+            $scope.getNewNumbers();
+        }
+    };
+
+    $scope.doToggleAnimation = function () {
+        if ($scope.animationToggle === '') {
             $scope.animationToggle = 'pointsAnimation';
         } else {
             $scope.animationToggle = '';
-        } 
+        }
     };
 
-    $scope.checkAnswer = function() {
+    $scope.checkAnswer = function () {
         // Using evil twins for comparing a number with a string
         if ($scope.givenAnswer == $scope.answer()) {
             $scope.message = 'Bra!';
-            $scope.first = $scope.getRandomNumber();
-            $scope.second = $scope.getRandomNumber();
+            $scope.getNewNumbers();
             $scope.nrOfCorrectAnswers++;
             $scope.doToggleAnimation();
             $scope.playRandomly();
@@ -48,5 +75,16 @@ function MathAppCtrl($scope) {
             $scope.message = 'Testa igen!';
         }
         $scope.givenAnswer = '';
+    };
+    
+    $scope.getLevelString = function () {
+        switch ($scope.level) {
+            case 1:
+                return 'Lätt (1 - 10)';
+            case 2:
+                return 'Mellan (1 - 20)';
+            case 3:
+                return 'Svår (1 - 100)';
+        }
     };
 }
